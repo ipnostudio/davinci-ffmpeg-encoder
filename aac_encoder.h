908f6280e39fa3aa1907c62aabebfb0b67f03df2
ipnostudio <ipno.studio01@gmail.com>
2026-05-31 02:41:27 -0600

    Update aac_encoder.h

--- aac_encoder.h
+++ aac_encoder.h
@@ -6,13 +6,12 @@
 extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libswresample/swresample.h>
-#include <libavutil/opt.h>
 #include <libavutil/channel_layout.h>
 }
 
 namespace IOPlugin {
 
-class AACEncoder : public IPluginCodecRef {
+class AACEncoder final : public IPluginCodecRef {
 public:
     static constexpr uint8_t UUID[16] = {
         0xfa, 0x3c, 0x11, 0xe8, 0x9a, 0x5b, 0x4b, 0xd1,
@@ -27,17 +26,17 @@ protected:
     StatusCode DoInit(HostPropertyCollectionRef* p_pProps) override;
     StatusCode DoOpen(HostBufferRef* p_pBuff) override;
     StatusCode DoProcess(HostBufferRef* p_pBuff) override;
-    void DoFlush() override;
+    void       DoFlush() override;
 
 private:
+    StatusCode ConvertAndEncode(const uint8_t* pcmData, int numSamples);
     StatusCode DrainReadyPackets();
     StatusCode DrainEncoder();
     StatusCode SendOutputPacket(AVPacket* pkt);
-    StatusCode ConvertAndEncode(const uint8_t* pcmData, int numSamples, int numChannels);
 
-    int sampleRate_  = 48000;
-    int bitRate_     = 320000;
-    int channels_    = 2;
+    int sampleRate_ = 48000;
+    int bitRate_    = 320000;
+    int channels_   = 2;
 
     AVCodecContext* ctx_    = nullptr;
     SwrContext*     swrCtx_ = nullptr;
@@ -45,7 +44,7 @@ private:
     AVPacket*       pkt_    = nullptr;
 
     std::vector<float> accumBuffer_;
-    int64_t ptsAccum_ = 0;
+    int64_t ptsAccum_  = 0;
     int     frameSize_ = 1024;
 };
 
