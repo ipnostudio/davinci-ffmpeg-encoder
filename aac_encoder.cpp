bb2841ff430f459d844a289dd578c72f65e9c624
Ipno Studio <ipno.studio01@gmail.com>
2026-06-05 16:34:18 -0600

    Update aac_encoder.cpp

--- aac_encoder.cpp
+++ aac_encoder.cpp
@@ -149,8 +149,7 @@ StatusCode AACEncoder::InitFFmpeg() {
     m_ctx->codecCtx->sample_fmt  = AV_SAMPLE_FMT_FLTP;
     m_ctx->codecCtx->sample_rate = (int)m_sampleRate;    
     m_ctx->codecCtx->strict_std_compliance = FF_COMPLIANCE_NORMAL;
-    //m_ctx->codecCtx->profile = FF_PROFILE_AAC_LOW;
-    m_ctx->codecCtx->profile = 1;
+    m_ctx->codecCtx->profile = FF_PROFILE_AAC_LOW;    
     
     av_channel_layout_default(&m_ctx->codecCtx->ch_layout, (int)m_numChannels);
 
